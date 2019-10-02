3e1e94a246a58fd290cf36f36a45bff3cf580b88
Harrypotterrrr <jiahaolin19971119@126.com>
2019-10-02 01:18:24 -0700

    add codes for backward check

--- benchmark.py
+++ benchmark.py
@@ -25,9 +25,10 @@ if __name__ == "__main__":
 
     globalContrast = GlobalContrast().cuda()
 
-    x = list(range(0, W * H)) * B * C
-    x = torch.tensor(x, dtype=torch.float64, requires_grad=True).cuda().reshape(B, C, W, H)
     # x = torch.rand((B, C, W, H), requires_grad=True).cuda()
+    x = list(range(0, W * H)) * B * C
+    x = torch.tensor(x, dtype=torch.float32, requires_grad=True).cuda().reshape(B, C, W, H)
+    x = torch.nn.Parameter(x) # for gradient check
 
     c = globalContrast(x)
 
@@ -43,13 +44,11 @@ if __name__ == "__main__":
         elapsed_fw = time.time() - start
         forward_time += elapsed_fw
 
-
         grad = torch.sum(c)
         start = time.time()
-        grad.backward()
+        grad.backward(retain_graph=True)
         elapsed_bk = time.time() - start
         backward_time += elapsed_bk
-
 
     forward_time *= 1000.0
     backward_time *= 1000.0
